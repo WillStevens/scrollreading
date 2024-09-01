7b0d914aa693a44f1aec28e754436754f4b23b35
WillStevens <will.stevens@srm.org.uk>
2024-09-01 20:49:55 +0100

    Added test to limit intersections when stitching

--- neighbours_in_vol.py
+++ neighbours_in_vol.py
@@ -31,27 +31,39 @@ for f1 in files1:
 
 pointsetsFiles1 = {}
   
+ptMult = 1024
 print("Loading volumes")
 for f1 in files1:
+  print(f1)
   pointset1 = set()
   with open(d1+"/v"+f1[1:]) as csvfile:
     volreader = csv.reader(csvfile)
     for row in volreader:
       pt = [int(s) for s in row]
-      pointset1.add( (pt[0],pt[1],pt[2]) )
-  pointset1.update( [(x+1,y,z) for (x,y,z) in pointset1] ,
-                    [(x-1,y,z) for (x,y,z) in pointset1] ,
-                    [(x,y+1,z) for (x,y,z) in pointset1] ,
-                    [(x,y-1,z) for (x,y,z) in pointset1] ,
-                    [(x,y,z+1) for (x,y,z) in pointset1] ,
-                    [(x,y,z-1) for (x,y,z) in pointset1] )
+      # represent each point as an integer
+      pointset1.add( ((pt[1]+1)*ptMult+pt[0]+1)*ptMult+pt[2]+1 )
+      pointset1.add( ((pt[1]+1+1)*ptMult+pt[0]+1)*ptMult+pt[2]+1 )
+      pointset1.add( ((pt[1]+1-1)*ptMult+pt[0]+1)*ptMult+pt[2]+1 )
+      pointset1.add( ((pt[1]+1)*ptMult+pt[0]+1+1)*ptMult+pt[2]+1 )
+      pointset1.add( ((pt[1]+1)*ptMult+pt[0]+1-1)*ptMult+pt[2]+1 )
+      pointset1.add( ((pt[1]+1)*ptMult+pt[0]+1)*ptMult+pt[2]+1+1 )
+      pointset1.add( ((pt[1]+1)*ptMult+pt[0]+1)*ptMult+pt[2]+1-1 )
+
   pointsetsFiles1[f1] = pointset1
 
 print("Finished loading volumes")
-		
+
+pointsets_xz_Files1 = {}
+
+for f1 in files1:
+  pointsets_xz_Files1[f1] = set([x % (ptMult*ptMult) for x in pointsetsFiles1[f1]])
+  		
 for i in range(0,len(files1)):
+  f1 = files1[i]
+  pointset1 = pointsetsFiles1[f1]
+  pointset1_xz = pointsets_xz_Files1[f1]
+
   for j in range(i+1,len(files1)):
-    f1 = files1[i]
     f2 = files1[j]
     if i != j:
 	  
@@ -88,12 +100,22 @@ for i in range(0,len(files1)):
       boundary = set()
 
       if overlap:
-        pointset1 = pointsetsFiles1[f1]
         pointset2 = pointsetsFiles1[f2]
-        boundary = pointset1.intersection(pointset2)
-			
-        if (len(boundary)>100):
-          matches += [( (f1[1:4],f1[5:-4]) , (f2[1:4],f2[5:-4]) ,len(boundary) )]
+        
+		# How many points in common?
+		
+        common = pointset1.intersection(pointset2)
+		
+        # Also work out how many points have x and z in common but not y - these are caused by intersections and we don't want those
+        pointset2_xz = pointsets_xz_Files1[f2]
+		
+        common_xz = pointset1_xz.intersection(pointset2_xz)
+	
+	    # These two tests aim to limit the number of intersections that we end up with
+		# Only join if the common boundary is more than 100 voxels, and if the number of intersection voxels is no more than a small constant
+		# times the boundary size.
+        if (len(common)>100 and len(common_xz) <= 4*len(common)):
+          matches += [( (f1[1:4],f1[5:-4]) , (f2[1:4],f2[5:-4]) ,len(common),len(common_xz) )]
           print(matches[-1])
 
 
@@ -102,7 +124,7 @@ imageSets = []
 
 # Need to do several iterations to make sure we have a disjoint list of sets
 for i in range(0,10):
-  for ((folder1,image1) , (folder2,image2), boundary) in matches:
+  for ((folder1,image1) , (folder2,image2), common,common_xz) in matches:
     addedToExisting = False
     for i in range(0,len(imageSets)):
       if (folder1,image1) in imageSets[i]:
