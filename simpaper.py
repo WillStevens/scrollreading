eeeb28271c6bc79cc0f02bdb3f821658b2840deb
WillStevens <will.stevens@srm.org.uk>
2025-04-03 23:48:39 +0100

    simpaper.py working well

--- simpaper.py
+++ simpaper.py
@@ -1,15 +1,17 @@
 import numpy as np
 from math import sqrt
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
 # We need a 3-d array where the first 2 dimensions correspond to x and y on a sheet of paper, and the 3rd dimension is of length 3, for
 # the coordinates of a point
 
-paperSize = (10,10)
+paperSize = (51,51)
 
-SPRING_FORCE_CONSTANT = 0.1
+SPRING_FORCE_CONSTANT = 0.2
 FRICTION_CONSTANT = 0.9
+GRAVITY_FORCE = 0.01
 
 paperPos = np.empty((paperSize[0],paperSize[1],3))
 paperVel = np.zeros_like(paperPos)
@@ -21,7 +23,7 @@ for x in range(0,paperSize[0]):
   for y in range(0,paperSize[1]):
     paperPos[x,y,0] = x
     paperPos[x,y,1] = y
-    paperPos[x,y,2] = 0
+    paperPos[x,y,2] = 50.0
 
 def Forces():
   global  paperAcc, paperVel
@@ -29,7 +31,8 @@ def Forces():
   paperAcc = np.zeros_like(paperPos)
   for x in range(0,paperSize[0]):
     for y in range(0,paperSize[1]):
-      for xo in range(x-1,x+2):
+     paperAcc[x,y,2] -= GRAVITY_FORCE
+     for xo in range(x-1,x+2):
         for yo in range(y-1,y+2):
           if xo>=0 and xo<paperSize[0]:
             if yo>=0 and yo<paperSize[1]:
@@ -55,12 +58,13 @@ def PlotIt(i):
 
   ax = plt.axes(projection='3d')
 
-  ax.set_xlim([-5,15])
-  ax.set_ylim([-5,15])
-  ax.set_zlim([-5,5])
+  ax.set_xlim([-1,51])
+  ax.set_ylim([-1,51])
+  ax.set_zlim([-1,51])
   
   # Creating array points
 
+  """
   xList = []
   yList = []
   zList = []
@@ -77,6 +81,9 @@ def PlotIt(i):
 
   # To create a scatter graph
   ax.scatter(xa,ya,za, c=ca)
+"""
+
+  ax.plot_surface(paperPos[:,:,0],paperPos[:,:,1],paperPos[:,:,2])
 
   # turn off/on axis
   plt.axis('on')
@@ -88,10 +95,16 @@ def PlotIt(i):
   #plt.show()
 
                 
-for i in range(0,40):
+for i in range(0,1000):
   Forces()
-  if i<20:
-    paperAcc[5,5,2] = 0.01
+#  if i<20:
+#    paperAcc[5,5,2] = 0.01
   paperVel += paperAcc
-  paperPos += paperVel   
+  paperPos += paperVel
+
+  for x in range(20,31):
+    for y in range(20,31):
+      if paperPos[x,y,2]<45.0:
+        paperPos[x,y,2]=45.0	  
+  
   PlotIt(i)
