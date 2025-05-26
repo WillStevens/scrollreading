5ce709fd746dbf0bd3d4a5f42c9a496a9e66f33b
WillStevens <will.stevens@srm.org.uk>
2025-05-26 23:31:59 +0100

    Add files via upload

--- simpaper/papervectorfield_closest.c
+++ simpaper/papervectorfield_closest.c
@@ -1,5 +1,6 @@
 #define VESUVIUS_IMPL
-#include "../vesuvius-c/vesuvius-c.h"
+#include "../../vesuvius-c/vesuvius-c.h"
+#include "../zarrlike/zarrlike.h"
 
 #define VF_RAD 2
 #define SORTED_DIST_SIZE ((VF_RAD*2+1)*(VF_RAD*2+1)*(VF_RAD*2+1)-1)
@@ -10,77 +11,127 @@
 
 #define SURFACE_VALUE 255
 
-float ****MakeFloatVectorArray(int x, int y, int z)
+int vol_start[3] = {5120,2048,3200};
+int chunk_dims[3] = {128,128,128};
+int chunk_start[3] = {-1,-1,-1};
+
+volume *scroll_vol;
+chunk *scroll_chunk = NULL;
+
+#define CHUNK_BUFFER_SIZE 8
+int cache_starts[CHUNK_BUFFER_SIZE][3];
+chunk *cache_chunks[CHUNK_BUFFER_SIZE];
+uint64_t cache_used[CHUNK_BUFFER_SIZE];
+uint64_t cache_counter;
+
+chunk *get_cached_chunk(int start[3], bool debug)
 {
-	float ****zarray = malloc(z*sizeof(float***));
-	
-	for(int zi=0; zi<z; zi++)
+	if (debug)
+		printf("GCC A\n");
+	for(int i = 0; i<CHUNK_BUFFER_SIZE; i++)
 	{
-      printf("%d\n",zi);
-	  zarray[zi] = malloc(y*sizeof(float**));
-	  for(int yi=0; yi<y; yi++)
-	  {
-		  zarray[zi][yi] = malloc(x*sizeof(float*));
-		  for(int xi=0; xi<x; xi++)
-		  {
-			  zarray[zi][yi][xi] = malloc(3*sizeof(float));
-			  zarray[zi][yi][xi][0]=0.0f;
-			  zarray[zi][yi][xi][1]=0.0f;
-			  zarray[zi][yi][xi][2]=0.0f;
-		  }
-	  }
+		if (cache_chunks[i] && cache_starts[i][0] == start[0] && cache_starts[i][1] == start[1] && cache_starts[i][2] == start[2])
+		{
+			cache_used[i] = cache_counter++;
+			if (debug)
+				printf("GCC B\n");
+			return cache_chunks[i];
+		}
+	}
+
+	if (debug)
+		printf("GCC C\n");
+	
+	for(int i = 0; i<CHUNK_BUFFER_SIZE; i++)
+	{
+		if (cache_chunks[i] == NULL)
+		{
+			if (debug)
+	    	    printf("GCC D\n");
+
+			int scroll_chunk_start[3] = {start[0]+vol_start[0],start[1]+vol_start[1],start[2]+vol_start[2]}; 
+			cache_chunks[i] = vs_vol_get_chunk(scroll_vol, scroll_chunk_start,chunk_dims);
+			cache_starts[i][0] = start[0];
+			cache_starts[i][1] = start[1];
+			cache_starts[i][2] = start[2];
+
+			cache_used[i] = cache_counter++;
+			if (debug)
+				printf("GCC E\n");
+
+			return cache_chunks[i];
+		}
+	}
+
+	if (debug)
+		printf("GCC F\n");
+
+	uint64_t oldest = cache_used[0];
+	int oldestIndex = 0;
+	for(int i = 1; i<CHUNK_BUFFER_SIZE; i++)
+	{
+		if (cache_used[i] < oldest)
+		{
+			oldest = cache_used[i];
+			oldestIndex = i;
+		}
 	}
 	
-	return zarray;
-}
-
-float ***MakeFloatArray(int x, int y, int z)
-{
-	float ***zarray = malloc(z*sizeof(float**));
+	int i = oldestIndex;
 	
-	for(int zi=0; zi<z; zi++)
+	if (debug)
+		printf("GCC G\n");
+	
+	// discard the oldest
+	vs_chunk_free(cache_chunks[i]);
+	int scroll_chunk_start[3] = {start[0]+vol_start[0],start[1]+vol_start[1],start[2]+vol_start[2]}; 
+	cache_chunks[i] = vs_vol_get_chunk(scroll_vol, scroll_chunk_start,chunk_dims);
+	if (cache_chunks[i] == NULL)
 	{
-	  zarray[zi] = malloc(y*sizeof(float*));
-	  for(int yi=0; yi<y; yi++)
-	  {
-		  zarray[zi][yi] = malloc(x*sizeof(float));
-		  for(int xi=0; xi<x; xi++)
-		  {
-			  zarray[zi][yi][xi] = 0.0f;
-		  }
-	  }
+		printf("ERROR - did not get chunk\n");
 	}
-	
-	return zarray;
+	cache_starts[i][0] = start[0];
+	cache_starts[i][1] = start[1];
+	cache_starts[i][2] = start[2];
+
+	cache_used[i] = cache_counter++;
+			
+	if (debug)
+		printf("GCC H\n");
+
+	return cache_chunks[i];	
 }
 
-
+// x,y,z are offset from vol_start
+unsigned char get_scroll_point(int z, int y, int x, bool debug)
+{
+	if (z<chunk_start[0] || z>=chunk_start[0]+chunk_dims[0] ||
+        y<chunk_start[1] || y>=chunk_start[1]+chunk_dims[1] ||
+        x<chunk_start[2] || x>=chunk_start[2]+chunk_dims[2])
+	{
+		if (debug)
+			printf("GSP A\n");
+		scroll_chunk = NULL;
+	}			
+	if (!scroll_chunk)
+	{
+		if (debug)
+			printf("GSP B\n");
+		chunk_start[0]=z-z%chunk_dims[0];
+		chunk_start[1]=y-y%chunk_dims[1];
+		chunk_start[2]=x-x%chunk_dims[2];
+		
+		scroll_chunk = get_cached_chunk(chunk_start,debug);
+	}
+	if (debug)
+		printf("GSP C\n");
+	
+	return vs_chunk_get(scroll_chunk,z%chunk_dims[0],y%chunk_dims[1],x%chunk_dims[2]);
+}
 
 int main() {
-/*
-    //pick a region in the scoll to visualize
-    int vol_start[3] = {3072,3072,3072};
-    int chunk_dims[3] = {128,512,512};
-    
-    //initialize the volume
-    volume* scroll_vol = vs_vol_new(
-        "./54keV_7.91um_Scroll1A.zarr/0/",
-        "https://dl.ash2txt.org/full-scrolls/Scroll1/PHercParis4.volpkg/volumes_zarr_standardized/54keV_7.91um_Scroll1A.zarr/0/");
-    
-    // get the scroll data by reading it from the cache and downloading it if necessary
-    chunk* scroll_chunk = vs_vol_get_chunk(scroll_vol, vol_start,chunk_dims);
-
-    // Fetch a slice  from the volume
-    slice* myslice = vs_slice_extract(scroll_chunk, 0);
-
-    // Write slice image to file
-    vs_bmp_write("xy_slice.bmp",myslice);
-*/
-
-    //pick a region in the scoll to visualize
-//    int vol_start[3] = {2432,2304,4096};
-    int vol_start[3] = {2432,2304,4096};
-    int chunk_dims[3] = {SIZE_Z,SIZE_Y,SIZE_X};
+    blosc2_init();
+	
     
     //initialize the volume
     /*
@@ -90,11 +141,12 @@ int main() {
     */
 
     	
-	volume* scroll_vol = vs_vol_new(
+	scroll_vol = vs_vol_new(
         "d:/zarrs/s1-surface-regular.zarr",
         "https://dl.ash2txt.org/community-uploads/bruniss/scrolls/s1/surfaces/full_scroll/s1-surface-erode.zarr/");
     
-
+	
+	
 	/*
 	volume* scroll_vol = vs_vol_new(
         "d:/zarrs/s1a-rectoverso-06032025-ome.zarr/0/",
@@ -102,9 +154,9 @@ int main() {
 	*/
 	
     // get the scroll data by reading it from the cache and downloading it if necessary
-    chunk* scroll_chunk = vs_vol_get_chunk(scroll_vol, vol_start,chunk_dims);
+    //scroll_chunk = vs_vol_get_chunk(scroll_vol, vol_start,chunk_dims);
 
-	
+	/*
 	for(int z=0; z<SIZE_Z; z++)
 	{
 		slice* myslice = vs_slice_extract(scroll_chunk, z);
@@ -115,22 +167,18 @@ int main() {
         // Write slice image to file
         vs_bmp_write(fname,myslice);
     }
-	
-	int vf_dims[3];
-	
-	for(int i = 0; i<3; i++)
-	  vf_dims[i] = chunk_dims[i]-2*VF_RAD;
+	*/
 
     /*
     static float vf[SIZE_Z-2*VF_RAD][SIZE_Y-2*VF_RAD][SIZE_X-2*VF_RAD][3];
     static float vf_smooth[SIZE_Z-2*VF_RAD][SIZE_Y-2*VF_RAD][SIZE_X-2*VF_RAD][3];
 	static float distance[SIZE_Z-2*VF_RAD][SIZE_Y-2*VF_RAD][SIZE_X-2*VF_RAD];
 	*/
-	printf("Allocating vf\n");
-	float ****vf = MakeFloatVectorArray(SIZE_Z-2*VF_RAD,SIZE_Y-2*VF_RAD,SIZE_X-2*VF_RAD);
-	printf("Allocating distance\n");
-	float ***distance = MakeFloatArray(SIZE_Z-2*VF_RAD,SIZE_Y-2*VF_RAD,SIZE_X-2*VF_RAD);
-	
+	printf("Creating ZARR\n");
+	printf("Vector field is in the following order: x,y,z,distance\n");
+	ZARR *vfz = ZarrOpen("d:/pvf_508.zarr");
+	ZARR *vfsz = ZarrOpen("d:/pvfs_508.zarr");
+		
     // Precompute nearest voxels and associated distance and direction vector
     float sortedDistances[SORTED_DIST_SIZE][7];
 	int i = 0;
@@ -177,115 +225,120 @@ int main() {
 
     printf("Calculating vector field...\n");
 
-    for(int z = 0; z<vf_dims[0]; z++)
+	float vf[4];
+	for(int zc = 0; zc < SIZE_Z-VF_RAD; zc+=CHUNK_SIZE_Z)
+	for(int yc = 0; yc < SIZE_Y-VF_RAD; yc+=CHUNK_SIZE_Y)
+	for(int xc = 0; xc < SIZE_X-VF_RAD; xc+=CHUNK_SIZE_X)
+	{
+	bool firstInChunk = true;
+	printf("Z:%d,Y:%d,X:%d\n",zc,yc,xc);
+    for(int z = zc<VF_RAD?VF_RAD:zc; z<zc+CHUNK_SIZE_Z && z<SIZE_Z-VF_RAD; z++)
 	{
         printf("%d\n",z);
-        for(int y = 0; y<vf_dims[1]; y++)
-        for(int x = 0; x<vf_dims[2]; x++)
+        for(int y = yc<VF_RAD?VF_RAD:yc; y<yc+CHUNK_SIZE_Y && y<SIZE_Y-VF_RAD; y++)
+        for(int x = xc<VF_RAD?VF_RAD:xc; x<xc+CHUNK_SIZE_X && x<SIZE_X-VF_RAD; x++)
 		{
-			vf[z][y][x][0]=0.0;
-			vf[z][y][x][1]=0.0;
-			vf[z][y][x][2]=0.0;
-			distance[z][y][x]=0.0;
-			
-            if (vs_chunk_get(scroll_chunk,z+VF_RAD,y+VF_RAD,x+VF_RAD) != SURFACE_VALUE)
+			vf[0]=0.0;
+			vf[1]=0.0;
+			vf[2]=0.0;
+			vf[3]=100.0; // distance
+		
+            if (get_scroll_point(z,y,x,false) != SURFACE_VALUE)
 			{
                 int minCount = 0;
                 float lastDist = 0;
                 for(int i = 0; i<SORTED_DIST_SIZE; i++)
-				{
-                    if (vs_chunk_get(scroll_chunk,z+VF_RAD+sortedDistances[i][3],y+VF_RAD+sortedDistances[i][2],x+VF_RAD+sortedDistances[i][1]) == SURFACE_VALUE)
+				{ 
+
+                    if (get_scroll_point(z+sortedDistances[i][3],y+sortedDistances[i][2],x+sortedDistances[i][1],false) == SURFACE_VALUE)
 					{
+
                         if (sortedDistances[i][0] != lastDist)
 						{
                             if (minCount>0)
 							{
-                                vf[z][y][x][0] /= minCount;
-                                vf[z][y][x][1] /= minCount;
-                                vf[z][y][x][2] /= minCount;
-								distance[z][y][x]=lastDist;
+                                vf[0] /= minCount;
+                                vf[1] /= minCount;
+                                vf[2] /= minCount;
+								vf[3]=lastDist;
+
+
+								if (firstInChunk)
+								{
+									ZarrWriteN(vfz,x,y,z,0,4,vf);
+									firstInChunk = false;
+								}
+								else
+								{
+									ZarrNoCheckWriteN(vfz,x,y,z,0,4,vf);
+								}
 
                                 break;
 							}
                             lastDist = sortedDistances[i][0];
 						}
-                        vf[z][y][x][0] += sortedDistances[i][4];
-                        vf[z][y][x][1] += sortedDistances[i][5];
-                        vf[z][y][x][2] += sortedDistances[i][6];
+                        vf[0] += sortedDistances[i][4];
+                        vf[1] += sortedDistances[i][5];
+                        vf[2] += sortedDistances[i][6];
                         minCount += 1;
 					}
 				}
 			}
 		}
-	}
+	}}
 
     printf("Smoothing vector field...\n");
 
-	float vf_smooth[3];
-	
-	FILE *f = fopen("vectorfield_test_smooth_508.csv","w");
-
+	float vf_smooth[4];
 	int window = 1;
-    for(int z = 0; z<vf_dims[0]; z++)
+	for(int zc = 0; zc < SIZE_Z-VF_RAD; zc+=CHUNK_SIZE_Z)
+	for(int yc = 0; yc < SIZE_Y-VF_RAD; yc+=CHUNK_SIZE_Y)
+	for(int xc = 0; xc < SIZE_X-VF_RAD; xc+=CHUNK_SIZE_X)
+	{
+	bool firstInChunk = true;
+	printf("Z:%d,Y:%d,X:%d\n",zc,yc,xc);	
+    for(int z = zc<VF_RAD?VF_RAD:zc; z<zc+CHUNK_SIZE_Z && z<SIZE_Z-VF_RAD; z++)
 	{
         printf("%d\n",z);
-        for(int y = 0; y<vf_dims[1]; y++)
-        for(int x = 0; x<vf_dims[2]; x++)
+        for(int y = yc<VF_RAD?VF_RAD:yc; y<yc+CHUNK_SIZE_Y && y<SIZE_Y-VF_RAD; y++)
+        for(int x = xc<VF_RAD?VF_RAD:xc; x<xc+CHUNK_SIZE_X && x<SIZE_X-VF_RAD; x++)
 		{
 			vf_smooth[0]=0.0;
 			vf_smooth[1]=0.0;
 			vf_smooth[2]=0.0;
 			int tot = 0;
-            for(int zo = (z-window<0?0:z-window); zo < (z+window+1<vf_dims[0]?z+window+1:vf_dims[0]); zo++)
-            for(int yo = (y-window<0?0:y-window); yo < (y+window+1<vf_dims[1]?y+window+1:vf_dims[1]); yo++)
-            for(int xo = (x-window<0?0:x-window); xo < (x+window+1<vf_dims[2]?x+window+1:vf_dims[2]); xo++)
+            for(int zo = (z-window<0?0:z-window); zo < (z+window+1<SIZE_Z-VF_RAD?z+window+1:SIZE_Z-VF_RAD); zo++)
+            for(int yo = (y-window<0?0:y-window); yo < (y+window+1<SIZE_Y-VF_RAD?y+window+1:SIZE_Y-VF_RAD); yo++)
+            for(int xo = (x-window<0?0:x-window); xo < (x+window+1<SIZE_X-VF_RAD?x+window+1:SIZE_X-VF_RAD); xo++)
 			{
-			    vf_smooth[0] += vf[zo][yo][xo][0];
-			    vf_smooth[1] += vf[zo][yo][xo][1];
-			    vf_smooth[2] += vf[zo][yo][xo][2];
+				ZarrReadN(vfz,xo,yo,zo,0,3,vf);
+			    vf_smooth[0] += vf[0];
+			    vf_smooth[1] += vf[1];
+			    vf_smooth[2] += vf[2];
                 tot ++;
 			}
 			
 			vf_smooth[0] /= tot;
 			vf_smooth[1] /= tot;
 			vf_smooth[2] /= tot;
+			// Get distance
+            vf_smooth[3] = ZarrRead(vfz,x,y,z,3);
 
-			fprintf(f,"%f,%f,%f,%f\n",vf_smooth[0],vf_smooth[1],vf_smooth[2],distance[z][y][x]);
+			if (firstInChunk)
+			{
+				ZarrWriteN(vfsz,x,y,z,0,4,vf_smooth);
+				firstInChunk = false;
+			}
+			else
+				ZarrNoCheckWriteN(vfsz,x,y,z,0,4,vf_smooth);
 		}
 	}
+	}
 	
-	fclose(f);
+	ZarrClose(vfz);
+	ZarrClose(vfsz);
 	
+	blosc2_destroy();
     return 0;
 }
 
-/*
-      
-for z in range(vfRad,size-vfRad-1):
-  print(z)
-  for y in range(vfRad,size-vfRad-1):
-    for x in range(vfRad,size-vfRad-1):
-      if k[z,y,x] != 255:
-        minCount = 0
-        lastDist = None
-        for (dist,xo,yo,zo,v) in sortedDistances:
-          if k[z+zo,y+yo,x+xo] == 255:
-            if dist != lastDist:
-              if minCount>0:
-                vf[z-vfRad,y-vfRad,x-vfRad,:] /= minCount
-                break
-              lastDist = dist
-            vf[z-vfRad,y-vfRad,x-vfRad,:] += v
-            minCount += 1
-      
-print("Outputting...")
-for i in range(0,vfSize):
-  j = Image.fromarray(vf[i,:,:,0]/10.0+0.5)
-  j.save("tmp\\papervectorfield_test_x_%03d.tif"%i)
-  j = Image.fromarray(vf[i,:,:,1]/10.0+0.5)
-  j.save("tmp\\papervectorfield_test_y_%03d.tif"%i)
-  j = Image.fromarray(vf[i,:,:,2]/10.0+0.5)
-  j.save("tmp\\papervectorfield_test_z_%03d.tif"%i)
-
-np.save("vectorfield_test_100.npy",vf)
-*/
